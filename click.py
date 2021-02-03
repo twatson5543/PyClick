7cec4ef31765bff2c1d4d264cbe04c20070cd5e3
Tristin <twatson5543@gmail.com>
2021-02-02 21:16:27 -0500

    Basic script

--- click.py
+++ click.py
@@ -1,7 +1,19 @@
 # Libraries
 import keyboard
 import time
+import mouse
+from pynput.mouse import Button, Controller
 
+mouse = Controller()
 # Infinite Loop for activation
-
+while True:
+    if keyboard.is_pressed('w'):
+        print("You pressed W")
+        time.sleep(0.05)
+    if keyboard.is_pressed('c'):
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+        time.sleep(0.02)
+    if keyboard.is_pressed('q'):
+        break
 # Parameters Adjustment
