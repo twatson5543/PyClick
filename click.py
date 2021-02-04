e4efcab1c1119f4a0245039137c899469d25f2f5
Tristin <twatson5543@gmail.com>
2021-02-04 00:53:03 -0500

    Add toggle, fix math

--- click.py
+++ click.py
@@ -6,14 +6,14 @@ from pynput.mouse import Button, Controller
 # Defining
 mouse = Controller()
 
-click_speed = 0.6     # in milliseconds
+click_speed = 5     # in milliseconds
 quit_key = 'q'        # Key to quit program
 click = 'c'           # Hold to click
 click_toggle = 'x'    # Press to toggle click on/off
 
 # Math
 
-real_clickspd = click_speed / 100
+real_clickspd = click_speed / 1000
 
 # Print to confirm
 
@@ -25,13 +25,29 @@ print("Real Click Speed: ",real_clickspd,"(s)")
 
 # Infinite Loop for activation
 while True:
-    if keyboard.is_pressed('w'):
-        print("You pressed W")
-        time.sleep(0.05)
+    #if keyboard.is_pressed('w'):
+    #    print("You pressed W")
+    #    time.sleep(0.05)
+
+    # Hold and thou shall click fast
     if keyboard.is_pressed(click):
         mouse.press(Button.left)
         mouse.release(Button.left)
         time.sleep(real_clickspd)
+
+    # Press and the gods shall click for you
+    if keyboard.is_pressed(click_toggle):
+        print("Click Loop Activated")
+        time.sleep(1)
+        while True:
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            time.sleep(real_clickspd)
+            if keyboard.is_pressed(click_toggle):
+                print("Done Clicking")
+                time.sleep(1)
+                break
+
+    # Click and thou shall exit
     if keyboard.is_pressed(quit_key):
         break
-# Parameters Adjustment
